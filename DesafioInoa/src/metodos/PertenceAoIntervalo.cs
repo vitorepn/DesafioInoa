04487333f40d3dc47029cc1124f494bfc206e810
vitorepn <vitorepn11@gmail.com>
2022-01-19 13:26:59 -0300

    Exceptions adicionadas

--- DesafioInoa/src/metodos/PertenceAoIntervalo.cs
+++ DesafioInoa/src/metodos/PertenceAoIntervalo.cs
@@ -8,9 +8,20 @@ namespace DesafioInoa.src.metodos
 {
     internal class PertenceAoIntervalo
     {
+        static Dictionary<string, float> Dados;
         public static int Executar(string Stock,float AlvoSuperior, float AlvoInferior)
         {
-            Dictionary<string, float> Dados = GetStock.Executar(Stock);
+            
+            try
+            {
+                Dados = GetStock.Executar(Stock);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ativo inválido");
+
+
+            }
             if (AlvoSuperior <= Dados["high"] && AlvoSuperior >= Dados["low"]) { 
                 return 1; 
             }else if(AlvoInferior <= Dados["high"] && AlvoInferior >= Dados["low"])
